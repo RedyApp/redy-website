340babe9e1f33769944cc36ca1b99cc5be799845
rigobertoGomez <rigobertogomez.gf@gmail.com>
2019-12-16 22:55:42 -0600

    🎨 add layout components

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="bg-redy-gray py-12">
+    <div class="container mx-auto">
+      <div class="flex justify-between items-center">
+        <p class="max-w-xs text-gray-500">
+          © Copyright 2019 Redy Technologies Inc.
+          Todos los derechos reservados.
+        </p>
+        <div class="flex justify-between items-center">
+          <div class="flex mr-6">
+            <nuxt-link to class="text-lg hover:text-redy-gold mr-8">Términos y condiciones</nuxt-link>
+            <nuxt-link to class="text-lg hover:text-redy-gold">Políticas y Privacidad</nuxt-link>
+          </div>
+          <div class="flex items-center mb-6 lg:mb-0">
+            <nuxt-link to="/" class="ml-6">
+              <i class="icon-instagram text-black hover:text-redy-gold text-2xl"></i>
+            </nuxt-link>
+            <nuxt-link to="/" class="ml-6">
+              <i class="icon-twitter text-black hover:text-redy-gold text-2xl"></i>
+            </nuxt-link>
+            <nuxt-link to="/" class="ml-6">
+              <i class="icon-facebook text-black hover:text-redy-gold text-2xl"></i>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="flex items-center justify-between py-6 px-12 absolute w-full z-50">
+    <div class="flex items-center">
+      <nuxt-link to class="mr-20">
+        <img src="/img/logo.svg" class="w-24" alt />
+      </nuxt-link>
+      <nuxt-link to class="font-semibold mr-8 flex items-center">
+        <i class="icon-arrow text-redy-gold mr-2"></i>Soporte Redy
+      </nuxt-link>
+      <nuxt-link to class="font-semibold flex items-center">
+        <i class="icon-arrow text-redy-gold mr-2"></i>Términos
+      </nuxt-link>
+    </div>
+    <div>
+      <div class="flex flex-col lg:flex-row items-center">
+        <p class="mr-4 font-semibold text-black text-lg mb-4 lg:mb-0">Muy pronto en</p>
+        <div class="flex items-center mb-20 lg:mb-0">
+          <nuxt-link
+            to="/"
+            class="w-12 h-12 rounded-full flex items-center justify-center mx-2 bg-redy-gold"
+          >
+            <i class="icon-apple text-black text-2xl"></i>
+          </nuxt-link>
+          <nuxt-link
+            to="/"
+            class="w-12 h-12 rounded-full flex items-center justify-center mx-2 bg-redy-gold"
+          >
+            <i class="icon-playstore text-black text-2xl"></i>
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: ''
+}
+</script>

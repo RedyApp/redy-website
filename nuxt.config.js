aacd3c0eb420baf6e6d8d6906fec3869b4595d73
rigobertoGomez <rigobertogomez.gf@gmail.com>
2020-01-03 10:04:25 -0600

    ✨ add OG image

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,7 +13,14 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      {
+        hid: 'title',
+        name: 'og:title',
+        property: 'og:title',
+        content: 'Redy App'
+      },
+      { hid: 'og:image', property: 'og:image', content: '/img/og-redy.png' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
